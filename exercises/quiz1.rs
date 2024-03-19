a7fe41d522db0198497257a2ace819c378adf97f
KiwoonKim <48469253+KiwoonKim@users.noreply.github.com>
2024-03-19 14:13:03 +0000

    Day1. set up a init and do some exercise til primitive_types3

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
+fn calculate_price_of_apples(quantity : u32) -> u32 {
+    if quantity > 40 {
+        quantity
+    } else {
+        quantity * 2
+    }
+}
 // fn calculate_price_of_apples {
-
 // Don't modify this function!
 #[test]
 fn verify_test() {
